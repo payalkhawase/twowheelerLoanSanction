f088a7da6aab6a7165db0c58ab328cda724eb2c0
GajananChavhan-pixel <shubhamchavhan077@gmail.com>
2025-02-26 21:50:31 +0530

    Added Pojo class

--- twowheelerLoanSanction/src/main/java/in/shriram/dreambiketwowheelerloan/sanction/model/CustomerDetails.java
+++ twowheelerLoanSanction/src/main/java/in/shriram/dreambiketwowheelerloan/sanction/model/CustomerDetails.java
@@ -1,10 +1,31 @@
 package in.shriram.dreambiketwowheelerloan.sanction.model;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class CustomerDetails {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+   private int sanctionedId;
+   private Date date;
+   private String applicantname;
+   private Double contactdetails;
+   private String onRoadPrice;
+   private double loanAmountScantioned;
+   private String interesType;
+   private float rateofInterest;
+   private int loanTenureMonth;
+   private double monthlyEmiAmount;
+   private String modeofPayment="Online";
+   private String status;
+   
+	
 }
