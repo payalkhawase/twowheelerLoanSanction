6654f74b967230e1ed101b94f2172b4576ec00a5
payalkhawase <payal.khawase14@gmail.com>
2025-03-05 22:34:32 +0530

    add

--- twowheelerLoanSanction/src/main/java/in/shriram/dreambiketwowheelerloan/sanction/model/Customer.java
+++ twowheelerLoanSanction/src/main/java/in/shriram/dreambiketwowheelerloan/sanction/model/Customer.java
@@ -6,6 +6,7 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 
@@ -64,7 +65,7 @@ public class Customer {
 	@OneToOne(cascade = CascadeType.ALL)
 	private LoanDisbursement loandisburst;
 	
-	@OneToOne(cascade = CascadeType.ALL)
+	@OneToMany(cascade = CascadeType.ALL)
 	private Ledger led;
 
 }
